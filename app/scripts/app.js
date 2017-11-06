c37a979d1559a2fdb35807666a719bdacbc86241
JJ Lau <j.j.j.lau@live.com>
2017-11-06 23:11:58 +0100

    Cleaned up uneeded files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,6 @@
  */
 angular
   .module('lifeazDemoApp', [
-    'chart.js',
     'ngAnimate',
     'ngCookies',
     'ngResource',
@@ -25,27 +24,7 @@ angular
         controller: 'DashboardCtrl',
         controllerAs: 'dashboard'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
       .otherwise({
         redirectTo: '/'
       });
-  })
-  .config(function(ChartJsProvider) {
-    // Configure all charts
-    ChartJsProvider.setOptions({
-      colors: ['#97BBCD', '#DCDCDC', '#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360']
-    });
-    // Configure all doughnut charts
-    ChartJsProvider.setOptions('doughnut', {
-      cutoutPercentage: 60
-    });
-    ChartJsProvider.setOptions('bubble', {
-      tooltips: {
-        enabled: false
-      }
-    });
   });
